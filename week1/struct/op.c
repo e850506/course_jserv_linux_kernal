90bdec36c38f3f99b790a08ec2902089f49186ba
unknowntpo <e850506@gmail.com>
2020-03-21 07:35:12 +0800

    Do some GDB test to verify the memory op of struct

---
+++ week1/struct/op.c
@@ -0,0 +1,8 @@
+/*
+ * @Title: Structure operator ->
+ * @Author: unknowntpo
+ * @Time: 2020.3.19 (Thu)
+ * @Short Description
+ *      Define a structure with members and observe its address and behavior of ->
+ */
+struct 
