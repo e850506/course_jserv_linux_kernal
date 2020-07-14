43a7e0b2877dcebfd15423b0485cb44af6a87e0a
unknowntpo <e850506@gmail.com>
2020-07-14 10:10:52 +0800

    Adding clang-format to automatically format code

--- sort/sort.c
+++ sort/sort.c
@@ -20,7 +20,7 @@ void swap(int *px, int *py)
     *py = tmp;
 }
 /** @brief This function sort the integer array passed in
- *  , which size is specified by the second parameter, 
+ *  , which size is specified by the second parameter,
  *  @param a A pointer to first element of an integer array.
  *  @param size specify the size of integer array a
  *  @return integer array a
@@ -43,23 +43,22 @@ int *sort_bubble(int *a, int size)
 void display(int *p, int size)
 {
     /* Display the array */
-    for(int i = 0; i < size; i++)
-        printf("%d", *p++);    
+    for (int i = 0; i < size; i++)
+        printf("%d", *p++);
     printf("\n");
 }
 
 int main()
 {
-    //int a[] = {3, 2, 1};
+    // int a[] = {3, 2, 1};
     int a[] = {8, 7, 6, 5, 4, 3, 2, 1};
-    //int a[] = {3, 1, 6, 5, 4, 2, 8, 7};
+    // int a[] = {3, 1, 6, 5, 4, 2, 8, 7};
     int size = 8;
     int *p = a;
     /* TODO: use callback function to apply more sort method */
     p = sort_bubble(a, size);
 
     display(p, size);
-    
-    return 0;
 
+    return 0;
 }
